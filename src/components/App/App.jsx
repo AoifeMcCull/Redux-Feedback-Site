6419942902a14fb79710bb54a368ed2bcb76c6cc
Jamie McCullough <92225779+KeeganMcCullough@users.noreply.github.com>
2024-10-12 09:03:37 -0500

    Added routing and initial Feeling.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+
 
 import Feeling from '../Feeling/Feeling';
 import Understanding from '../Understanding/Understanding';
 import Support from '../Support/Support';
 import Comments from '../Comments/Comments';
 import Review from '../Review/Review';
+
 
 function App() {
 
@@ -16,6 +20,16 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Feeling} />
+          <Route path='/Understanding' component={Understanding} />
+          <Route path='/Support' component={Support} />
+          <Route path='/Comments' component={Comments} />
+          <Route path='/Review' component={Review} />
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 function Feeling(){
+    const dispatch = useDispatch();
+    const history = useHistory();
+    let [feelingToSend, setFeelingToSend] = useState(5)
+
+    const handleFeelingChange = (event) => {
+        setFeelingToSend(event.target.value)
+    }
+
+    const addFeeling = (event) => {
+        event.preventDefault();
+        dispatch({
+            type:'setFeeling',
+            payload: Number(feelingToSend)
+        })
+        history.push('/Understanding')
+    }
+
+
     return(
         <div>
-
+            <p>Feeling</p>
+            <form onSubmit={(event) => addFeeling(event)}>
+                <input onChange={handleFeelingChange} type='text' placeholder='Feeling'></input>
+                <button type='submit' data-testid='next'>Next</button>
+            </form>
         </div>
     )
 }
